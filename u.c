2106017810fb508f4cef29c791a8040277a6facf
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-01-29 13:57:22 +0530

    Create u.c

---
+++ u.c
@@ -0,0 +1,13 @@
+#include <stdio.h> #include<conio.h> void main() { int i;
+
+printf("Enter a number: \n");
+scanf("%d", &i);
+if (i > 0)
+    printf("%d is positive number \n", i);
+else if (i < 0)
+    printf("%d is negative number \n", i);
+
+    printf("0 is neither positive nor negative");
+
+    getch();
+}
