2402b83bf556474bb7d2b14b020fc7d2d1476eae
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-01-31 15:06:23 +0530

    Update and rename set14.c to set15.c

---
+++ set15.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    double a,b,c;
+
+    printf("Enter three numbers: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+ if( a>=b && a>=c)
+        printf("%.2lf is the largest number.", a);
+else if (n2>=n1 && n2>=n3)
+        printf("%.2lf is the largest number.", b);
+    else
+        printf("%.2lf is the largest number.", c);
+    return 0;
+}
